2efce1f994b083d6e9da92a0105689f67174fe86
Loik <loikchoua4@gmail.com>
2019-10-31 20:17:46 -0300

    Ingreso de la IP como cadena

--- ClienteSideApp.c
+++ ClienteSideApp.c
@@ -6,6 +6,7 @@
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <arpa/inet.h>
+#include <ctype.h>
 
 
 int main(int argc, char const *argv[]){
@@ -18,7 +19,7 @@ int main(int argc, char const *argv[]){
 
     start =1100; //Puerto de incio del escaneo
     end=1200;   //Puerto de fin del escaneo
-
+/*
     //Ingreso la IP con verficacion que no se ingresen numeros invalidos
     do{
         printf("\n----------------------------Ingrese la direccion IP del Servidor----------------------------\n ");
@@ -26,68 +27,55 @@ int main(int argc, char const *argv[]){
     }while (serverIPAddress[0]<0 || serverIPAddress[0]>255 || serverIPAddress[1]<0 || serverIPAddress[1]>255 || serverIPAddress[2]<0 || serverIPAddress[2]>255 || serverIPAddress[3]<0 || serverIPAddress[3]>255);
     printf("La direccion IP ingresada es %d.%d.%d.%d\n",serverIPAddress[0],serverIPAddress[1],serverIPAddress[2],serverIPAddress[3]);
     
-    for(i=0; i<7; i=i+2){
-        int x = i+1;
-        int j = i/2;
-        hostname[i]=serverIPAddress[j];
-        hostname[x]= '.';
+    for(i=0; i<=3; i=i+1){
+        itoa(serverIPAddress[i],hostname,10);
+        printf("%c", hostname[i]);
     }
-
-    strncpy((char*) & amp;sa , "" , sizeof sa);
+*/
+    printf("Ingrese la direccion IP: ");
+    gets(hostname);
+    //strncpy((char*) & amp;sa , "" , sizeof sa);
     sa.sin_family = AF_INET;
     if(isdigit(hostname[0])){
-        printf("Doing inet_addr...");
         sa.sin_addr.s_addr = inet_addr(hostname);
-        printf("Done\n");
     }
 
         printf("Starting the portscan loop : \n");
-    for( i = start ; i &lt;= end ; i++) 
+    for( i = start ; i <= end ; i++) 
     {
         //Fill in the port number
         sa.sin_port = htons(i);
         //Create a socket of type internet
-        sock = socket(AF_INET , SOCK_STREAM , 0);
+        origin_socket = socket(AF_INET , SOCK_STREAM , 0);
          
-        //Check whether socket created fine or not
-        if(sock &lt; 0) 
+        //Check whether socket created fine or nots
+        if(origin_socket < 0) 
         {
+            printf("Error en puerto %d", i);
             perror("\nSocket");
             exit(1);
+        }else{
+
         }
         //Connect using that socket and sockaddr structure
-        err = connect(sock , (struct sockaddr*)&amp;sa , sizeof sa);
+    
+        err = connect(origin_socket , (struct sockaddr*)&amp;sa , sizeof sa);
          
         //not connected
-        if( err &lt; 0 )
+        if( err < 0 )
         {
             //printf("%s %-5d %s\r" , hostname , i, strerror(errno));
-            fflush(stdout);
+            close(origin_socket);
+            continue;
         }
         //connected
         else
         {
             printf("%-5d open\n",  i);
         }
-        close(sock);
+        close(origin_socket);
     }
-
-    //printf("\n Conversion de la direccion: %d",inet_addr(hostname));
    
-
-
-
-    //Creando el Socket Origen
-    /*
-	origin_socket = socket(AF_INET , SOCK_STREAM , IPPROTO_IP);   //El primer parametro indica la Suite TCP/IP, el segundo que es un Socket de flujo,
-                                                                // y el tercero que utilizamos el protocolo IP
-    
-
-    if (origin_socket == -1)
-        {
-            printf("No se pudo crear el Socket");
-        }
-    */
     return 0;
 }
 
