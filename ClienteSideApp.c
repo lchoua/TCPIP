68d275c98f91aa0a30627a55e6b3a2cd224759b2
Loik <loikchoua4@gmail.com>
2019-11-01 10:59:08 -0300

    Escaneo de puertos

--- ClienteSideApp.c
+++ ClienteSideApp.c
@@ -14,9 +14,8 @@ int main(int argc, char const *argv[]){
     int origin_socket, err,i, start, end;
     int serverIPAddress[4] = {};
     char hostname[100];
-    struct sockaddr_in sa;
+    struct sockaddr_in sa = {0};
     struct hostent *host;
-
     start =1100; //Puerto de incio del escaneo
     end=1200;   //Puerto de fin del escaneo
 /*
@@ -34,46 +33,64 @@ int main(int argc, char const *argv[]){
 */
     printf("Ingrese la direccion IP: ");
     gets(hostname);
-    //strncpy((char*) & amp;sa , "" , sizeof sa);
-    sa.sin_family = AF_INET;
-    if(isdigit(hostname[0])){
-        sa.sin_addr.s_addr = inet_addr(hostname);
-    }
 
-        printf("Starting the portscan loop : \n");
+   //strncpy((char*)(struct sockaddr*) &sa , "" , sizeof sa);
+   //memccpy(&sa.sin_addr, hostname, sizeof(hostname));
+   
+    
+
+    printf("\nEmpezando el escaneo de puertos: \n");
     for( i = start ; i <= end ; i++) 
     {
-        //Fill in the port number
-        sa.sin_port = htons(i);
-        //Create a socket of type internet
+        //Creamos el Socket para IPv4 del tipo Stream
         origin_socket = socket(AF_INET , SOCK_STREAM , 0);
-         
+
+        //Chequeo que el usuario haya ingresado una direccion IP y la asigno en la estructura
+        if(isdigit(hostname[0])){
+            sa.sin_addr.s_addr = inet_addr(hostname);
+        }
+        //En la estructura asigno IPv4
+        sa.sin_family = AF_INET;
+        //Selecciono el puerto a verificar
+        sa.sin_port = htons(i);
+        printf("\nComprobando puerto %d\n", i);
+        //Establezo la conexion con el Socket
+        err = connect(origin_socket ,(struct sockaddr *) &sa , sizeof (sa));
+        //Si la conexion fue exitosa muestro que puerto esta abierto, al encontrar un puerto abierto finalizo el escaneo
+        if(err != -1){
+            char host[128];
+            char service[128];
+            getnameinfo((struct sockaddr *)&sa, sizeof sa, host, (sizeof host), service, sizeof service, 0);
+            printf("\nPort : %d, Service : %s, Open\n", i, service);
+            exit(1); 
+        }
+        close(origin_socket);
+        
+
+
+        /*
         //Check whether socket created fine or nots
         if(origin_socket < 0) 
         {
-            printf("Error en puerto %d", i);
-            perror("\nSocket");
-            exit(1);
-        }else{
-
+            printf("\nError en puerto %d",i);
         }
         //Connect using that socket and sockaddr structure
     
-        err = connect(origin_socket , (struct sockaddr*)&amp;sa , sizeof sa);
-         
+        
         //not connected
         if( err < 0 )
         {
-            //printf("%s %-5d %s\r" , hostname , i, strerror(errno));
+            printf("Error al conectar");
+            printf("%s %-5d %s\r" , hostname , i, strerror(errno));
             close(origin_socket);
-            continue;
         }
         //connected
         else
         {
-            printf("%-5d open\n",  i);
+            printf("%-5d open\n",i);
         }
-        close(origin_socket);
+        */
+        //close(origin_socket);
     }
    
     return 0;
